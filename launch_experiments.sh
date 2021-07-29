0ae40192d47c3ecabb6443a37830c52ce6dff24d
jf-garamendi <jf.garamendi@upf.edu>
2021-07-29 15:59:18 +0200

    Exp_0005

--- launch_experiments.sh
+++ launch_experiments.sh
@@ -20,12 +20,20 @@
 
 # Exp 3º
 #python main.py configs/n0013_ag001_resnet152.json
-python main.py configs/n0014_ag001_resnet152.json
-python main.py configs/n0015_ag001_resnet152.json
+# python main.py configs/n0014_ag001_resnet152.json
+# python main.py configs/n0015_ag001_resnet152.json
 #python main.py configs/n0016_ag001_resnet152.json
 
 #Exp 2
-python main.py configs/n0009_ag001_resnet152.json
-python main.py configs/n0010_ag001_resnet152.json
-python main.py configs/n0011_ag001_resnet152.json
-python main.py configs/n0012_ag001_resnet152.json
+# python main.py configs/n0009_ag001_resnet152.json
+# python main.py configs/n0010_ag001_resnet152.json
+# python main.py configs/n0011_ag001_resnet152.json
+# python main.py configs/n0012_ag001_resnet152.json
+
+# Exp 5
+python main.py configs/n0023_Ag001_resnext101_32x8d.json
+python main.py configs/n0024_Ag001_wide_resnet101_2.json
+python main.py configs/n0025_Ag001_inception_v3.json
+python main.py configs/n0026_Ag001_densenet161.json
+python main.py configs/n0027_Ag001_vgg16_bn.json
+python main.py configs/n0028_Ag001_mnasnet1_0.json
