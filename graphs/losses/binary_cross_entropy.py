70ddb6e36e813801095a734e12d5af8acee59f51
jf-garamendi <jf.garamendi@upf.edu>
2021-07-30 19:18:40 +0200

    refactor

--- graphs/losses/binary_cross_entropy.py
+++ graphs/losses/binary_cross_entropy.py
@@ -6,7 +6,7 @@ import torch
 import torch.nn as nn
 
 
-class BinaryCrossEntropy(nn.Module):
+class Binary_Cross_Entropy(nn.Module):
     def __init__(self, config=None):
         super().__init__()
         self.loss = nn.BCELoss()
