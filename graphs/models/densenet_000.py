717c3da54b14cd9455032e7107425cd78fe8cbed
jf-garamendi <jf.garamendi@upf.edu>
2021-08-06 17:12:55 +0200

    logging

--- graphs/models/densenet_000.py
+++ graphs/models/densenet_000.py
@@ -19,6 +19,8 @@ class Densenet_000(nn.Module):
             # or fine tuning
             for param in self.backbone.parameters():
                 param.requires_grad = False
+
+            self.logger.info('Freezing convolutional part \n')
         else:
             self.logger.info('Training from scratch \n')
 
