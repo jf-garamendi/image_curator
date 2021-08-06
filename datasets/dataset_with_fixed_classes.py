ce1fddef23be9a5c2d61730e48594abf64942a57
Adrià Ciurana <info@dreamlearning.ai>
2021-08-06 15:51:54 +0200

    Fix

--- datasets/dataset_with_fixed_classes.py
+++ datasets/dataset_with_fixed_classes.py
@@ -1,5 +1,8 @@
+
 from typing import Optional, Callable, Any
+
 from torchvision.datasets.folder import ImageFolder, IMG_EXTENSIONS, default_loader
+from torchvision.models.resnet import resnet101
 
 class CustomImageFolder(ImageFolder):
     def __init__(
@@ -10,10 +13,18 @@ class CustomImageFolder(ImageFolder):
             loader: Callable[[str], Any] = default_loader,
             is_valid_file: Optional[Callable[[str], bool]] = None,
     ):
-        super().__init__(root, transform, target_transform, loader, IMG_EXTENSIONS if is_valid_file is None else None)
+        super(CustomImageFolder, self).__init__(
+            root=root, 
+            transform=transform, 
+            target_transform=target_transform, 
+            loader=loader, 
+            is_valid_file=None
+        )
 
-    def find_classes(self, directory):
+
+    def _find_classes(self, directory):
         return (
             ['rejected', 'approved'],
             {'rejected': 0, 'approved': 1}
-        ),
+        )
+        
